3730a4ac3658c166d3ae525145855eb79fcc0d87
AsumVictor <iamasum369@gmail.com>
2024-08-31 01:03:19 +0000

    Added

---
+++ src/linkedlist/singlyCircularLinkedList/SingleCircularLinkedList.java
@@ -0,0 +1,33 @@
+package linkedlist.singlyCircularLinkedList;
+
+import linkedlist.SingleLinkedList.ListNode;
+
+public class SingleCircularLinkedList {
+     public ListNode head;
+     private int size = 0;
+     public ListNode tail;
+
+     public boolean isEmpty(){
+         return size == 0;
+     }
+
+     public void insert(int data){
+         ListNode newNode = new ListNode(data);
+         if(isEmpty()){
+             head = newNode;
+             tail = head;
+             return;
+         }
+         tail.next = newNode;
+         newNode.next = head;
+         tail = newNode;
+     }
+
+     public void printEl(){
+         ListNode currNode = head;
+
+         while()
+     }
+
+
+}
